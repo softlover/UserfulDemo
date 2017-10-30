2495f1f205f778849af2a31a1b680791695ba3f0
softlover <mynamelibing@163.com>
2017-10-30 18:15:31 +0800

    添加Fragmeng基类实现生命周期

--- app/src/main/java/com/mydo/demo/base/BaseFragment.java
+++ app/src/main/java/com/mydo/demo/base/BaseFragment.java
@@ -1,13 +1,79 @@
 package com.mydo.demo.base;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
- * Fragmeng基类
+ * Fragment基类
  * @author: LiBing.
  * @date: 2017/10/30.
  * @version: V1.0.0.
  */
 
 public class BaseFragment extends Fragment {
+
+    public BaseFragment() {
+        super();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
 }
