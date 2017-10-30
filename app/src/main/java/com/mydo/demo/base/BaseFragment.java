a30edfd864d4456c4011349971f5ec522b334e2d
softlover <mynamelibing@163.com>
2017-10-30 18:08:07 +0800

    添加Fragment基类

---
+++ app/src/main/java/com/mydo/demo/base/BaseFragment.java
@@ -0,0 +1,13 @@
+package com.mydo.demo.base;
+
+import android.app.Fragment;
+
+/**
+ * Fragmeng基类
+ * @author: LiBing.
+ * @date: 2017/10/30.
+ * @version: V1.0.0.
+ */
+
+public class BaseFragment extends Fragment {
+}
